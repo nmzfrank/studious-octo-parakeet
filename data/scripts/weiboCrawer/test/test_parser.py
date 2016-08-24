171fa20eff0d1fc6e6f0065c1d5cb1be3259d116
nmzfrank <nmzfrank@sjtu.edu.cn>
2016-08-24 18:16:52 +0800

    daily sync

--- data/scripts/weiboCrawer/test/test_parser.py
+++ data/scripts/weiboCrawer/test/test_parser.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 
 
 class Spider(threading.Thread):
+
     def __init__(self, url, browser, threadLock):
         threading.Thread.__init__(self)
         self.url = url
@@ -24,11 +25,12 @@ class Spider(threading.Thread):
             elem = self.browser.find_element_by_css_selector("h1")
         except:
             print "unable to find h1 element"
-        print elem.text
-        
-
-
-
+        else:
+            print elem.text
+        self.browser.close()
+        handles = self.browser.window_handles
+        self.browser.switch_to_window(handles[0])
+        self.threadLock.release()
 
 
 def patchParsing(urls):
@@ -36,5 +38,12 @@ def patchParsing(urls):
     browser.get("http://baike.baidu.com/view/1136308.htm")
     threadLock = threading.Lock()
     for url in urls:
-        Spider(url,browser,threadLock)
-        Spider.start()
+        Spider(url, browser, threadLock).start()
+
+
+if __name__ == '__main__':
+    urls = []
+    for i in range(10):
+        url = "http://baike.baidu.com/view/" + str(i) + ".htm"
+        urls.append(url)
+    patchParsing(urls)
